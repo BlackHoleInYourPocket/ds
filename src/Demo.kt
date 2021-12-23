dbd26c8368aef7a29d08e9eebe304ff0a66da12a
Seyfi Can Zeyrek <seyficanzeyrek@gmail.com>
2021-12-23 14:34:14 +0300

    Tree

--- src/Demo.kt
+++ src/Demo.kt
@@ -2,13 +2,11 @@ package linkedlist
 
 import linkedlist.queueu.ArrayListQueue
 import linkedlist.queueu.LinkedListQueue
-import linkedlist.trees.TreeNode
-import linkedlist.trees.makeBeverageTree
 import stack.Stack
 
 fun main(args: Array<String>) {
     //Test here
-    val singleLinkedList = SingleLinkedList<Int>().apply { }
+    val singleLinkedList = SingleLinkedList<Int>()
     val doubleLinkedList = DoubleLinkedList<Int>().apply { }
     val stack = Stack<Int>().apply { }
     val arrayListQueue = ArrayListQueue<Int>().apply { }
